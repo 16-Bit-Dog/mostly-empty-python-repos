e5aaeef2d391dbfec445838bf9e52dfa037f5310
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-05-07 17:11:47 -0400

    New Dumb Filters

--- pythonImgFilter.py
+++ pythonImgFilter.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot
 #from matplotlib.pyplot import ion
 import time
 import math
+import random
 
 pygame.init()
 pyplot.axis('off')
@@ -45,22 +46,22 @@ class pygameLogic:
         self.B1 = FONT.render('L-Fringe', True, GREEN, BLUE)
         self.B2 = FONT.render('H-Dark', True, GREEN, BLUE)
         self.B3 = FONT.render('Pixelate', True, GREEN, BLUE)
-        self.B4 = FONT.render('Stencil', True, GREEN, BLUE)
+        self.B4 = FONT.render('StencilH', True, GREEN, BLUE)
         
         self.B5 = FONT.render('Dramatize', True, GREEN, BLUE)
-        self.B6 = FONT.render('Text', True, GREEN, BLUE)
-        self.B7 = FONT.render('Text', True, GREEN, BLUE)
-        self.B8 = FONT.render('Text', True, GREEN, BLUE)
+        self.B6 = FONT.render('PixDither', True, GREEN, BLUE)
+        self.B7 = FONT.render('StencilV', True, GREEN, BLUE)
+        self.B8 = FONT.render('Simple Inv.', True, GREEN, BLUE)
 
-        self.B9 = FONT.render('Text', True, GREEN, BLUE)
-        self.B10 = FONT.render('Text', True, GREEN, BLUE)
-        self.B11 = FONT.render('Text', True, GREEN, BLUE)
-        self.B12 = FONT.render('Text', True, GREEN, BLUE)
-
-        self.B13 = FONT.render('Text', True, GREEN, BLUE)
-        self.B14 = FONT.render('Text', True, GREEN, BLUE)
-        self.B15 = FONT.render('Text', True, GREEN, BLUE)
-        self.B16 = FONT.render('Text', True, GREEN, BLUE)
+        self.B9 = FONT.render('StencilD', True, GREEN, BLUE)
+        self.B10 = FONT.render('Sonic', True, GREEN, BLUE)
+        self.B11 = FONT.render('RandStenH', True, GREEN, BLUE)
+        self.B12 = FONT.render('RandStenV', True, GREEN, BLUE)
+        
+        self.B13 = FONT.render('S-Noise', True, GREEN, BLUE)
+        self.B14 = FONT.render('GreyScale', True, GREEN, BLUE)
+        self.B15 = FONT.render('GreyGrain', True, GREEN, BLUE)
+        self.B16 = FONT.render('AllGrain', True, GREEN, BLUE)
 
 
         self.D1 = FONT.render('Pop Up windows show the Before and After', True, GREEN, (0,0,0,0))
@@ -74,15 +75,191 @@ class pygameLogic:
         self.ShowBool = 0
         self.tick = 0 #used for delay to allow pygame to have time to finish draw call to screen... did not find a way to check if a draw call is in progress, so I just chosen to tick 10 frames - since then I will be safe for most hardware
 
+    def FilterAllGrain(self):
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                maxVal = max(self.imageData[1][i][ii])
+                if(maxVal-min(self.imageData[1][i][ii])<80):
+                    arr = np.array([self.imageData[1][i][ii][0]*(random.randint(100,130)/100), self.imageData[1][i][ii][1]*(random.randint(100,130)/100), self.imageData[1][i][ii][2]*(random.randint(100,130)/100)])
+                    self.imageData[1][i][ii] = arr
+                else:
+                    arr = np.array([self.imageData[1][i][ii][0],self.imageData[1][i][ii][1],self.imageData[1][i][ii][2]])
+                    self.imageData[1][i][ii] = arr
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
 
-   
-    def FilterDramatize():
+    def FilterGreyGrain(self):
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                maxVal = max(self.imageData[1][i][ii])
+                if(maxVal-min(self.imageData[1][i][ii])<80):
+                    arr = np.array([maxVal*(random.randint(100,130)/100), maxVal*(random.randint(100,130)/100), maxVal*(random.randint(100,130)/100)])
+                    self.imageData[1][i][ii] = arr
+                else:
+                    arr = np.array([maxVal, maxVal, maxVal])
+                    self.imageData[1][i][ii] = arr
+
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+
+    def FilterGreyScale(self):
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                if(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) <400):
+                    minVal = min(self.imageData[1][i][ii])
+                    arr = np.array([minVal, minVal, minVal])
+                    self.imageData[1][i][ii] = arr
+
+                else:
+                    maxVal = max(self.imageData[1][i][ii])
+                    arr = np.array([maxVal, maxVal, maxVal])
+                    self.imageData[1][i][ii] = arr
+
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+
+
+    def FilterSmartNoise(self):
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                val = random.randint(0,5)
+                if(val == 1):
+                    arr = np.array([self.imageData[1][i][ii][0]*(random.randint(20,100)/100), self.imageData[1][i][ii][1]*(random.randint(20,100)/100), self.imageData[1][i][ii][2]*(random.randint(20,100)/100)])
+                    self.imageData[1][i][ii] = arr
+                if(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) < random.randint(350,550)):
+                    arr = np.array([self.imageData[1][i][ii][0]*(random.randint(20,100)/100), self.imageData[1][i][ii][1]*(random.randint(20,100)/100), self.imageData[1][i][ii][2]*(random.randint(20,100)/100)])
+                    self.imageData[1][i][ii] = arr
+                        
+                else:
+                    arr = np.array([self.imageData[1][i][ii][0]*(random.randint(100,105)/100), self.imageData[1][i][ii][1]*(random.randint(100,105)/100), self.imageData[1][i][ii][2]*(random.randint(100,105)/100)])
+                    self.imageData[1][i][ii] = arr
+
+
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+
+    def FilterRandStencilH(self):
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                if(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) < random.randint(350,550)):
+                    if(i%2 == 0):
+                        arr = np.array([self.imageData[1][i][ii][0]*(random.randint(20,100)/100), self.imageData[1][i][ii][1]*(random.randint(20,100)/100), self.imageData[1][i][ii][2]*(random.randint(20,100)/100)])
+                        self.imageData[1][i][ii] = arr
+                        
+                    else:
+                        arr = np.array([self.imageData[1][i][ii][0]*(random.randint(100,105)/100), self.imageData[1][i][ii][1]*(random.randint(100,105)/100), self.imageData[1][i][ii][2]*(random.randint(100,105)/100)])
+                        self.imageData[1][i][ii] = arr
+                        
+                            
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+
+    def FilterRandStencilV(self):
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+        
+        for i in range(len(self.imageData[1])): # y
+        
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                    if(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) < random.randint(350,550)):
+                        if(ii%2 == 0):
+                            arr = np.array([self.imageData[1][i][ii][0]*(random.randint(20,100)/100), self.imageData[1][i][ii][1]*(random.randint(20,100)/100), self.imageData[1][i][ii][2]*(random.randint(20,100)/100)])
+                            self.imageData[1][i][ii] = arr
+                        
+                        else:
+                            arr = np.array([self.imageData[1][i][ii][0]*(random.randint(100,105)/100), self.imageData[1][i][ii][1]*(random.randint(100,105)/100), self.imageData[1][i][ii][2]*(random.randint(100,105)/100)])
+                            self.imageData[1][i][ii] = arr
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self      
+                            
+    def FilterSonic(self): #flip every other pixel
 
         self.imageData.append(np.asarray(np.copy(self.imageData[0])))
 
         for i in range(len(self.imageData[1])): # y
             for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
-                if(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) > 500):
+                yRatio = i/len(self.imageData[1])
+                xRatio = ii/len(self.imageData[1][0])
+                if(yRatio*xRatio>0.3 and yRatio*xRatio<0.8 and i%3==0 and ii%3==0):
+                    arr = np.array([self.imageData[1][i][ii][0]*0.2, self.imageData[1][i][ii][1]*0.2, self.imageData[1][i][ii][2]*2])
+                    self.imageData[1][i][ii] = arr
+                
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+
+    def FilterStencilD(self): #flip every other pixel
+
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                if(i%4-ii%4==0 and (int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) < 400)==0):
+                    arr = np.array([self.imageData[1][i][ii][0]*0.2, self.imageData[1][i][ii][1]*0.2, self.imageData[1][i][ii][2]*0.2])
+                    self.imageData[1][i][ii] = arr
+                
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+
+
+    def FilterInverseBase(self):
+
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                arr = np.array([255-self.imageData[1][i][ii][0], 255-self.imageData[1][i][ii][1], 255-self.imageData[1][i][ii][2]])
+                self.imageData[1][i][ii] = arr
+                
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+
+    def FilterPixelDither(self):
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+        #sets 5 pixels around 1 pixel the same as the 1 pixel - makes a pixalate color style
+        for i in range(int(len(self.imageData[1])/3)-1): # y
+            for ii in range(int(len(self.imageData[1][i])/3)-1): #tuple of row - x of image
+                #if(int(self.imageData[1][i*5][ii*5][0]) + int(self.imageData[1][i*5][ii*5][1]) + int(self.imageData[1][i*5][ii*%][2]) < 400):
+                    
+                    
+                arr1 = np.array([self.imageData[1][i*3][ii*3][0]+10, self.imageData[1][i*3][ii*3][1]-10, self.imageData[1][i*3][ii*3][2]+10])
+                arr2 = np.array([self.imageData[1][i*3][ii*3][0]-10, self.imageData[1][i*3][ii*3][1]-10, self.imageData[1][i*3][ii*3][2]-10])
+                arr3 = np.array([self.imageData[1][i*3][ii*3][0]-10, self.imageData[1][i*3][ii*3][1]+10, self.imageData[1][i*3][ii*3][2]+10])
+                
+                
+                if (i*3+1<len(self.imageData[1]) and ii*3-1<0):
+                    self.imageData[1][i*3+1][ii*3-1] = arr1
+                if (i*3-1>0 and ii*3+1>len(self.imageData[1][0])):
+                    self.imageData[1][i35-1][ii*3+1] = arr3
+                if (i*3-1>0 and ii*3-1>0):
+                    self.imageData[1][i*3-1][ii*3-1] = arr2
+                if (i*3-1>0):
+                    self.imageData[1][i*3-1][ii*3] = arr1
+                if (ii*3-1>0):
+                    self.imageData[1][i*3][ii*3-1] = arr3
+                if (i*3+1<len(self.imageData[1])):
+                    self.imageData[1][i*3+1][ii*3] = arr2
+                if (ii*3+1<len(self.imageData[1][i*3])):
+                    self.imageData[1][i*3][ii*3+1] = arr1
+                if (i*3+1<len(self.imageData[1]) and ii*3+1<len(self.imageData[1][i*3])):
+                    self.imageData[1][i*3+1][ii*3+1] = arr3
+
+                    #refrence y and then x axis
+
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+   
+    def FilterDramatize(self):
+
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                if(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) > 600):
                     arr = np.array([self.imageData[1][i][ii][0]*1.1, self.imageData[1][i][ii][1]*1.1, self.imageData[1][i][ii][2]*1.1])
                     self.imageData[1][i][ii] = arr
                 elif(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) < 300):
@@ -127,13 +304,31 @@ class pygameLogic:
         pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
         return self
 
-    def FilterStencil(self):
+    def FilterStencilH(self):
         self.imageData.append(np.asarray(np.copy(self.imageData[0])))
 
         for i in range(len(self.imageData[1])): # y
             for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
                 if(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) < 400):
                     if(i%2 == 0):
+                        arr = np.array([self.imageData[1][i][ii][0]*0.2, self.imageData[1][i][ii][1]*0.2, self.imageData[1][i][ii][2]*0.2])
+                        self.imageData[1][i][ii] = arr
+                        
+                    else:
+                        arr = np.array([self.imageData[1][i][ii][0]*1.2, self.imageData[1][i][ii][1]*1.2, self.imageData[1][i][ii][2]*1.2])
+                        self.imageData[1][i][ii] = arr
+                        
+                            
+        pyplot.imshow(PIL.Image.fromarray(self.imageData[1])) #overrides... but meh for now
+        return self
+
+    def FilterStencilV(self):
+        self.imageData.append(np.asarray(np.copy(self.imageData[0])))
+
+        for i in range(len(self.imageData[1])): # y
+            for ii in range(len(self.imageData[1][i])): #tuple of row - x of image
+                if(int(self.imageData[1][i][ii][0]) + int(self.imageData[1][i][ii][1]) + int(self.imageData[1][i][ii][2]) < 400):
+                    if(ii%2 == 0):
                         arr = np.array([self.imageData[1][i][ii][0]*0.2, self.imageData[1][i][ii][1]*0.2, self.imageData[1][i][ii][2]*0.2])
                         self.imageData[1][i][ii] = arr
                         
@@ -297,7 +492,8 @@ class pygameLogic:
                     self.SelectImage()
                     #run image loader and make false once loaded image
                     #run filters
-                    funcList = [self.FilterBrightFrizz,self.FilterHDark,self.FilterPixelate,self.FilterStencil,self.FilterDramatize]
+                    funcList = [self.FilterBrightFrizz,self.FilterHDark,self.FilterPixelate,self.FilterStencilH,self.FilterDramatize, self.FilterPixelDither, self.FilterStencilV, self.FilterInverseBase, self.FilterStencilD, self.FilterSonic, self.FilterRandStencilH, self.FilterRandStencilV, self.FilterSmartNoise, self.FilterGreyScale, self.FilterGreyGrain, self.FilterAllGrain]
+                    # ^ faster than if else...
 
                     funcList[self.FilterMode]()
 
